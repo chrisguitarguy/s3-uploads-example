fa50ad86a17287e5e12379060d4f581f0e2fa13a
Christopher Davis <cdavis9999@gmail.com>
2018-06-15 14:47:27 -0500

    Include the Body Parser Middlware
    we'll need it later on.

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const parser = require('body-parser');
 
 const app = express();
 
 app.use(morgan('tiny'));
+app.use(parser.json());
 app.use(express.static('./'));
 
-app.listen(8080);
+const server = app.listen(8080, function () {
+    const addy = server.address();
+    console.log(`listening on http://${addy.host || '0.0.0.0'}:${addy.port}`);
+});
