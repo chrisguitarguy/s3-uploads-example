df93ba58eed7abdf8b75f5607af7b95f515107c2
Christopher Davis <cdavis9999@gmail.com>
2018-06-15 20:02:28 -0500

    Use a Variable for the S3 Bucket Name

--- infrastructure.tf
+++ infrastructure.tf
@@ -8,8 +8,14 @@ variable "cors_origins" {
     default = ["http://localhost:8080"]
 }
 
+variable "bucket_name" {
+    type = "string"
+    description = "The name of the uploads bucket"
+    default = "chrisguitarguy-s3-uploads-tutorial"
+}
+
 resource "aws_s3_bucket" "upload" {
-    bucket = "chrisguitarguy-s3-uploads-tutorial"
+    bucket = "${var.bucket_name}"
     cors_rule = {
         allowed_origins = ["${var.cors_origins}"]
         allowed_headers = ["*"]
