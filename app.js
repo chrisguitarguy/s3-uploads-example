f2985169ddaefee3e7ad7fdac3d5b812ef35e244
Christopher Davis <cdavis9999@gmail.com>
2018-06-15 19:03:31 -0500

    Make the Client Side Uploads Work

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const uuid4 = require('uuid/v4');
 const bucket = 'chrisguitarguy-s3-uploads-tutorial';
 const expires = 60 * 5;
 const app = express();
-const s3 = new aws.S3();
+const s3 = new aws.S3({
+    // this is required for presigned URLs + CORs requests, apparently
+    // https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-streaming.html
+    signatureVersion: 'v4',
+});
 
 app.use(morgan('tiny'));
 app.use(express.json());
@@ -15,19 +19,30 @@ app.use(express.static('./'));
 // you may want to do something like send the original filename to this endpoint
 // to include in the generated file name or to figure out an extension.
 app.post('/presign', function (req, res) {
+    const filename = uuid4();
     s3.getSignedUrl('putObject', {
         Bucket: bucket,
-        Key: uuid4(),
+        Key: filename,
         Expires: expires,
     }, function (err, url) {
         if (err) {
             return res.status(500).json({error});
         }
 
-        res.status(201).json({url});
+        res.status(201).json({
+            url,
+            filename,
+        });
     });
 });
 
+app.post('/submit', function (req, res) {
+    console.log(req.body);
+
+    return res.status(200).json({
+        filename: req.body.filename,
+    });
+});
 
 const server = app.listen(8080, function () {
     console.log('server running', server.address());
--- client.js
+++ client.js
@@ -1,5 +1,56 @@
 (function (exports) {
+    function createPresignedUrl() {
+        return fetch('/presign', {method: 'POST'}).then(r => r.json());
+    }
+
+    function uploadFile(url, file) {
+        return fetch(url, {
+            method: 'PUT',
+            body: file,
+        }).then(resp => {
+            return resp.text().then(body => {
+                if (!resp.ok) {
+                    return Promise.reject({
+                        status: resp.status,
+                        body,
+                    });
+                }
+
+                return {
+                    status: resp.status,
+                    body,
+                };
+            });
+        });
+    }
+
     exports.onSubmit = function onSubmit(form) {
+        if (form.file.files.length < 1) {
+            alert('select a file please');
+            return false;
+        }
+
+
+        var file = form.file.files[0]
+
+        createPresignedUrl().then(res => {
+            // upload the file first
+            return uploadFile(res.url, file).then(() => res.filename);
+        }).then(filename => {
+            // now we know the filename of what got uploaded, send that
+            // to the form in place of the actual file
+            return fetch('/submit', {
+                method: 'POST',
+                body: JSON.stringify({filename}),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }).then(() => {
+                alert(`Hooray you uploaded ${filename}`);
+                form.reset();
+            });
+        });
+
         return false;
     }
 }(window));
